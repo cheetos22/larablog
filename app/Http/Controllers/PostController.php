5ad7e7bfa210e8078fbfc826dfd6ff6db53d3fd2
cheetos22 <cheetos22@gmail.com>
2021-09-22 17:20:44 +0200

    add show method form PostController and add post view

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -13,4 +13,9 @@ class PostController extends Controller
 
         return view('pages.posts', compact('posts'));
     }
+
+    public function show(Post $post){
+
+        return view('pages.post', compact('post'));
+    }
 }
