dca39cf24cc8a892931d162bb6e29a99dc0e545b
cheetos22 <cheetos22@gmail.com>
2021-09-25 17:45:30 +0200

    add RouteKeyName to Post model and excerpt attribute

--- app/Post.php
+++ app/Post.php
@@ -16,9 +16,21 @@ class Post extends Model
         'title', 'content', 'date', 'type',' image',
     ];
 
+    protected $dates = ['date'];
+
     public function setTitleAttribute($value)
     {
         $this->attributes['title'] = $value;
         $this->attributes['slug'] = Str::slug($value);
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function getExcerptAttribute()
+    {
+        return Str::limit(strip_tags($this->content), 350);
     }
 }
