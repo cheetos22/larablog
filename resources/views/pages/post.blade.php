3fc5c286cc67c8fd3e18513761b767a001259826
cheetos22 <cheetos22@gmail.com>
2021-11-29 13:26:35 +0100

    add comments section to view

--- resources/views/pages/post.blade.php
+++ resources/views/pages/post.blade.php
@@ -72,4 +72,36 @@
     </div>
 </article>
 @endif
+<section class="comments">
+    <div class="wrapper">
+        @if ($post->comments->count() > 0)
+        <div class="rte">
+            <h2>{{ $post->comments->count() }} {{ $post->comments->count() > 1 ? 'Comments' : 'Comments' }}</h2>
+        </div>
+        <div class="comments-list">
+            @foreach ($post->comments as $comment)
+            <article class="comment">
+                <div class="comment-meta">
+                    <img src="/images/avatar.jpg" alt="{{ $comment->author->name }}" class="comment-avatar">
+                    <span class="comment-user">{{ $comment->author->name }}</span>
+                    <span class="comment-date">{{ $comment->date->format('d.m.Y') }}</span>
+                </div>
+                <p class="comment-body rte">{{ $comment->content }}</p>
+            </article>
+            @endforeach
+        </div>
+        @endif
+        <div class="comments-add">
+            <div class="rte">
+                <h2>Add comment</h2>
+            </div>
+            <form action="#">
+                <div class="form-fieldset is-full">
+                    <textarea name="message" class="form-textarea" placeholder="Your comment"></textarea>
+                </div>
+                <button class="button">Submit</button>
+            </form>
+        </div>
+    </div>
+</section>
 @endsection
