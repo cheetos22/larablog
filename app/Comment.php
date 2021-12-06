826c5bcc64c3ad91838c4881d6708135a2c29dfb
cheetos22 <cheetos22@gmail.com>
2021-12-06 13:02:06 +0100

    add filable field to Comment model

--- app/Comment.php
+++ app/Comment.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
+    protected $fillable = [
+        'post_id', 'user_id', 'content',
+    ];
+
     protected $dates = ['date'];
 
     public function post()
